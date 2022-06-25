15df22e625761dd1f9e7161c559112b0d5822f47
thomaseasy1 <57138761+thomaseasy1@users.noreply.github.com>
2022-06-25 15:31:53 -0300

    Add files via upload

---
+++ projeto/projeto/Formulas.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto;
+
+/**
+ *
+ * @author Windows
+ */
+public abstract class Formulas {
+    public abstract float calcular(float num1,float num2);
+
+}
