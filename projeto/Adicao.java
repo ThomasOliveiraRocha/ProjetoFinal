74e7365748e3f5a253d7da44912f3094fb889faa
thomaseasy1 <57138761+thomaseasy1@users.noreply.github.com>
2022-06-26 20:05:18 -0300

    Add files via upload

---
+++ projeto/Adicao.java
@@ -0,0 +1,9 @@
+
+package projeto;
+
+
+public class Adicao extends Formulas{
+     public float calcular(float num1,float num2){
+        return num1 + num2;
+    }
+}
---
+++ projeto/Subtracao.java
@@ -0,0 +1,12 @@
+
+package projeto;
+
+
+public class Subtracao extends Formulas{
+   
+    public float calcular(float num1,float num2){
+        return num1 - num2;
+    }
+  
+   
+}
